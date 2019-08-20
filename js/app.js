4e612daf74bf64c6aed0e59db13ba0bd39a7bb25
Alex Brown <abrown@qz.com>
2019-08-20 16:31:03 -0400

    styling

--- js/app.js
+++ js/app.js
@@ -12,56 +12,225 @@ const headerTemplate = ({name, title, email}) => `
 function populateSocial(social) {
 	social.forEach(function(account, n) {
 		$('#social').append(`
-			<a href="${account}" target="_blank" class="social-account social-account-${n}"></a
+			<a href="${account.link}" target="_blank" class="social-account social-account-${n}">
+				${account.icon}
+			</a
 		`);
 	});
 }
 
 // Profile Template
-const profileTemplate = ({title, location}) => `
-	<div class="title">${title}</div>
+const profileTemplate = ({title, location, copy}) => `
+	<div class="portrait"></div>
 	<div class="text">
-		<div class="location">Location: ${location}</div>
+		<div class="location"><span class="bold">Location</span>: ${location}</div>
 		<div id="years" class="years"></div>
-		<div id="intro-copy" class="copy"></div.
+		<div id="intro-copy" class="copy">${copy}</div.
 	</div>
 	<div class="photo"></div>
-`
+`;
 
-//Populate Intro Copy
-function populateIntroCopy(copy) {
-	copy.forEach(function(paragraph, n) {
-		$('#intro-copy').append(`
-			<div class="paragraph">${paragraph}<div>
+// Experience Template
+const experienceTemplate = ({title}) => `
+	<div class="title">${title}</div>
+	<div id="jobs" class="jobs"></div>
+`;
+
+function vidHover() {
+	const section = $(this).attr('data-section');
+	const vidNum = $(this).attr('data-value');
+	const video = document.getElementById(`video-${section}-${vidNum}`); 
+	video.play();
+}
+
+function vidLeave() {
+	const section = $(this).attr('data-section');
+	const vidNum = $(this).attr('data-value');
+	const video = document.getElementById(`video-${section}-${vidNum}`); 
+	video.pause();
+}
+
+// Populate Jobs
+function populateJobs(jobs) {
+	jobs.forEach(function(job, n) {
+		$('#jobs').append(`
+			<a href="${job.url}" target="_blank" class="job" data-value="${n}" data-section="job">
+				<video id="video-job-${n}" muted playsinline loop>
+					<source src="${job.video}" type="video/mp4">
+				</video>
+				<div class="video-mask">
+					<div class="company"></div>
+					<div class="position">${job.position}</div>
+					<div class="role">${job.role}</div>
+				</div>
+			</a>
+		`)
+	});
+
+	$('.job').on('mouseover', vidHover);
+	$('.job').on('mouseleave', vidLeave);
+}
+
+// Education Template
+const educationTemplate = ({title}) => `
+	<div class="title">${title}</div>
+	<div id="schools" class="schools"></div>
+`;
+
+// Populate Schools
+function populateSchools(schools) {
+	schools.forEach(function(school, n) {
+		$('#schools').append(`
+			<a href="${school.url}" target="_blank" class="school school-${n}" data-value="${n}" data-section="school">
+				<video id="video-school-${n}" muted playsinline loop>
+					<source src="${school.video}" type="video/mp4">
+				</video>
+				<div class="school-image school-image-${n}"></div>
+				<div class="school-copy">
+					<div class="name">${school.name}</div>
+					<div class="location">${school.location}</div>
+					<div class="degree">${school.degree}</div>
+				</div>
+			</a>
 		`);
 	});
+
+	$('.school').on('mouseover', vidHover);
+	$('.school').on('mouseleave', vidLeave);
+}
+
+// Portfolio Template
+const portfolioTemplate = ({title}) => `
+	<div class="title">${title}</div>
+	<div id="projects" class="projects"></div>
+`;
+
+// Populate Projects
+function populateProjects(projects) {
+	projects.forEach(function(project, n) {
+		$('#projects').append(`
+			<a href="${project.url}" target="_blank" class="project project-${n}" data-value="${n}" data-section="project">
+				<div class="vid-container">
+					<video id="video-project-${n}" muted playsinline loop>
+						<source src="${project.video}" type="video/mp4">
+					</video>
+				</div>
+				<div class="title">${project.title}</div>
+			</a>
+		`);
+	});
+
+	$('.project').on('mouseover', vidHover);
+	$('.project').on('mouseleave', vidLeave);
 }
 
 // Set experience
-const experience = (start) => `Experience: ${today.getFullYear() - start} years`;
+const experience = (start) => `<span class="bold">Experience</span>: ${today.getFullYear() - start} years`;
 
 // Add HTML
-function populateHtml({header, profile}) {
+function populateHtml(data) {
 
 	// Add header
-	$('#header').append(headerTemplate(header));
+	$('#header').append(headerTemplate(data[0]));
 
 	// Add Social
-	populateSocial(header.social);
+	populateSocial(data[0].social);
 
 	// Add profile
-	$('#profile').append(profileTemplate(profile));
-
-	// Add Intro Copy
-	populateIntroCopy(profile.copy);
+	$('#profile').append(profileTemplate(data[1]));
 
 	// Add years of experience
-	$('#years').append(experience(profile.start));
+	$('#years').append(experience(data[1].start));
+
+	//Add Experience Content
+	$('#experience').append(experienceTemplate(data[2]));
+
+	// Add Jobs
+	populateJobs(data[2].jobs);
+
+	// Add Education Content
+	$('#education').append(educationTemplate(data[3]));
+
+	// Add Schools
+	populateSchools(data[3].schools);
+
+	// Add Portfolio
+	$('#portfolio').append(portfolioTemplate(data[4]));
+
+	// Add projects
+	populateProjects(data[4].projects);
+}
+
+let bgNum = 19;
+
+// Build background
+// function populateBg() {
+// 	for(var i = 0; i < bgNum; i++) {
+// 		$('#bg').append(`<div id="bg-strip-${i}" class="bg-strip bg-strip-${i}"></div`)
+// 	}
+// }
+
+let siteHeight;
+let screenHeight;
+let heightDiff;
+let diffRatio;
+let scrollStart = 0;
+let randomArray = [];
+let opacityStart = 1;
+
+// On Scroll
+
+function portfolioScroll() {
+	const scrollTop = $(this).scrollTop();
+	const scrollChange = scrollTop - scrollStart;
+	const opacityChange = opacityStart - (scrollChange/800);
+	// opacityStart = opacityChange
+	// console.log(scrollChange)
+	// scrollStart = scrollTop;
+	bgSet(scrollChange);
+	// $('#bg').css('opacity', opacityChange);
+
+}
+
+// On Scroll
+function screenSet() {
+	siteHeight = $('#portfolio-container').height();
+	screenHeight = $(window).height();
+	heightDiff = siteHeight - screenHeight;
+	diffRatio = heightDiff/100;
+}
+
+// Set BG
+
+const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight;
+
+function bgSet(topPos) {
+	var canvas = document.getElementById('bg');
+	var ctx = canvas.getContext('2d');
+	// const hover;
+	canvas.setAttribute('width', windowWidth);
+	canvas.setAttribute('height', windowHeight);
+	ctx.translate(0,0);
+	// ctx.rotate(angle*Math.PI/180);
+	ctx.fillStyle = 'rgba(0,0,0,1)';
+	for (var i = 0; i < windowHeight / 10; i++) {
+		const rectY = (i * 10);
+		for (var j = 0; j < windowWidth / 10; j++) {
+			let rectX = (j * 10);
+			// console.log(j % 2);
+			ctx.fillStyle = `rgba(191,135,15,${(i * 2)/windowWidth}`;
+			ctx.fillRect(rectX, rectY, 3, 3);
+		}
+	}
 }
 
 // App to run
 function on() {
+	// populateBg();
 	populateHtml(data);
+	bgSet(0);
+	$(document).on('scroll', portfolioScroll);
 }
 
 on();
--- js/data.js
+++ js/data.js
@@ -1,27 +1,155 @@
-const data = {
-	header: {
+const data = [
+	{
+		title: 'Header',
 		name: 'Alex Brown',
 		title: 'Web Developer',
 		social: [
-			'mailto:brown.alex.k@gmail.com',
-			'//github.com/wolfofwashington',
-			'//www.linkedin.com/in/alexbrowndeveloper/',
-			'//www.instagram.com/mister.alex.b/',
-			'//www.facebook.com/alex.k.brown',
-			'//twitter.com/WolfofWA'
+			{
+				link: 'mailto:brown.alex.k@gmail.com',
+				icon: '<i class="fas fa-envelope"></i>'
+			},
+			{
+				link: '//github.com/wolfofwashington',
+				icon: '\uf09b'
+			},
+			{
+				link: '//www.linkedin.com/in/alexbrowndeveloper/',
+				icon: 'linked-in-in'
+			},
+			{
+				link: '//www.instagram.com/mister.alex.b/',
+				icon: 'instagram'
+			},
+			{
+				link: '//www.facebook.com/alex.k.brown',
+				icon: 'facebook-f'
+			},
+			{
+				link: '//twitter.com/WolfofWA',
+				icon: 'twitter'
+			}
 		]
 	},
-	profile: {
+	{
 		title: 'Profile',
 		location: 'New York, NY',
-		start: 2015,
-		copy: [
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae interdum sapien, ut pulvinar nisl. Donec lobortis nec dui non sagittis. In fermentum libero et arcu pharetra varius. Integer finibus luctus condimentum. Curabitur pellentesque ex sit amet aliquet dignissim. Nam ac consequat justo. Aenean pharetra euismod fringilla. Curabitur id sagittis libero. Sed vitae elementum erat, eu pulvinar tortor. Duis at lorem vitae ante accumsan gravida non vel nulla. Aliquam gravida, mauris nec dictum aliquet, ante justo ullamcorper libero, in finibus nisi libero nec dolor. Curabitur posuere, justo vel laoreet euismod, lorem dui efficitur velit, vitae fringilla lectus lorem sed tortor. Quisque sed ipsum quis quam hendrerit malesuada a id nisi.',
-			'Cras feugiat est vitae rhoncus semper. Aenean bibendum ligula quam, ut mollis nisl tempus vel. Vestibulum nec imperdiet augue. Duis sollicitudin elit sed purus consequat dictum. Quisque mollis vulputate magna, non viverra sapien. In nec ante odio. Mauris in pulvinar odio, sed faucibus lacus.',
-			'Donec feugiat odio et ante feugiat fringilla. Integer nec ultrices orci. Ut urna odio, condimentum a odio porttitor, venenatis egestas libero. Cras commodo ante sit amet libero dignissim, sit amet dapibus tellus congue. Ut quis laoreet lacus. Aenean iaculis ipsum vitae posuere molestie. Sed porta ipsum eu feugiat mattis.'
-		] 
+		start: 2014,
+		copy: 'Cras feugiat est vitae rhoncus semper. Aenean bibendum ligula quam, ut mollis nisl tempus vel. Vestibulum nec imperdiet augue. Duis sollicitudin elit sed purus consequat dictum. Quisque mollis vulputate magna, non viverra sapien. In nec ante odio. Mauris in pulvinar odio, sed faucibus lacus.'
 	},
-	footer: {
-		
+	{
+		title: 'Experience',
+		jobs: [
+			{
+				company: 'Quartz',
+				position: 'Web Developer',
+				url: 'https://qz.com/',
+				role: 'Built custom interactive experiences & ads for clients with integrated tracking. Created & maintained internal tools such as updating Quartz’s video libraries. Conceived & built new features & experiences (360 video, force touch & machine learning). Built & maintained campaigns contributing $5.2 million of revenue in 2017. Presented to Deloitte Global’s upper management in their Quarterly Business Update. Built experiences for JPMC, Deloitte, HBO, Mercedes, Microsoft, Adobe, Bulgari & more.',
+				video: 'resources/videos/nyc.mp4'
+			},
+			{
+				company: 'Lift Media',
+				position: 'Web Developer/Designer',
+				url: 'http://liftmedia.io/',
+				role: 'Custom built & designed mobile responsive websites & other projects for clients. Designed with Adobe Illustrator & Adobe Photoshop. Managed client relationships. Worked with clients such as Xero, Purple, Room Choice, eLearning Bros & more.',
+				video: 'resources/videos/provo.mp4'
+			}
+		]
+	},
+	{
+		title: 'Education',
+		schools: [
+			{
+				name: 'Brigham Young University',
+				location: 'Provo, UT',
+				degree: 'Bachelor\'s of Arts, History',
+				url: 'https://www.byu.edu/',
+				video: 'resources/videos/byu.mp4'
+			},
+			{
+				name: 'V School',
+				location: 'Provo, UT',
+				degree: 'Full-Stack Development Bootcamp',
+				url: 'https://vschool.io/',
+				video: 'resources/videos/vschool.mp4'
+			}
+		]
+	},
+	{
+		title: 'Portfolio',
+		projects: [
+			{
+				title: 'Deloitte Interactive Bulletin',
+				url: 'https://qz.com/1123158/meet-your-future-workforce/',
+				video: 'resources/videos/deloitte.mp4'
+			},
+			{
+				title: 'JPMC Emerging Tech Interactive Bulletin',
+				url: 'https://qz.com/1119009/emerging-technology-unlocks-new-employment- opportunities/',
+				video: 'resources/videos/jpmc.mp4'
+			},
+			{
+				title: 'JPMC Clean Energy Interactive Bulletin',
+				url: 'https://qz.com/1073207/businesses-emerge-as-clean-energy-leaders-around-the- world/',
+				video: 'resources/videos/jpmc2.mp4'
+			},
+			{
+				title: 'Synchrony Moments Interactive Bulletin',
+				url: 'https://qz.com/1024947/in-retail-its-not-who-you-are-but-what-you-want-that- matters/',
+				video: 'resources/videos/synch.mp4'
+			},
+			{
+				title: 'Mercedes 360 Experience Ad',
+				url: 'http://ads.quartz.cc/sponsors/mercedes/interior-night/2016-07-15/',
+				video: 'resources/videos/mercedes.mp4'
+			},
+			{
+				title: 'HPE Pointnext Ad',
+				url: 'https://ads.quartz.cc/sponsors/hpe/pointnext-display/2017-04-01/',
+				video: 'resources/videos/hpe.mp4'
+			},
+			{
+				title: 'Citrix Red Bull Racing Ad',
+				url: 'https://ads.quartz.cc/sponsors/citrix/redbull-display/2017-06-15/',
+				video: 'resources/videos/redbull.mp4'
+			},
+			{
+				title: 'Lift Media Website',
+				url: 'http://liftmedia.io/',
+				video: 'resources/videos/liftmedia.mp4'
+			},
+			{
+				title: 'Room Choice Web App',
+				url: 'https://roomchoice.com/',
+				video: 'resources/videos/roomchoice.mp4'
+			},
+			{
+				title: 'Student Leasing Services Website',
+				url: 'https://slshouses.com/',
+				video: 'resources/videos/sls.mp4'
+			},
+			{
+				title: 'Success Partners Website',
+				url: 'https://www.successpartners.com/',
+				video: 'resources/videos/successpartners.mp4'
+			},
+			{
+				title: 'Purple Mattresses Custom Shopify eCommerce Page',
+				url: 'https://purple.com/',
+				video: 'resources/videos/purple.mp4'
+			},
+			{
+				title: 'iTovi Website',
+				url: 'https://itovi.com/',
+				video: 'resources/videos/itovi.mp4'
+			},
+			{
+				title: 'Dr. Allen Orthodontics Website',
+				url: 'https://drallenortho.com/',
+				video: 'resources/videos/allen.mp4'
+			}
+		]
+	},
+	{
+		title: 'Footer'	
 	}
-}
+]
